fd36f2a89ccaba3157027482573610d4cdfe8b97
Eadweard <804875728@qq.com>
2022-12-08 14:50:04 +0800

    container插槽组件

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import "~/styles/components/Container.scss"
 
-function Container() {
-	return (<div></div>)
+interface Props {
+	children: React.ReactNode[] | React.ReactNode
+}
+
+function Container(props: Props) {
+	return (<div className="container_box">
+		{props.children}
+	</div>)
 }
 
 export default Container
--- src/pages/Fabric.tsx
+++ src/pages/Fabric.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useEffect} from 'react';
 import '../styles/Fabric.scss'
 import {Button} from "@arco-design/web-react";
+import Container from "../components/Container";
 
 function Fabric() {
 	const [count, setCount] = React.useState(0);
@@ -12,9 +13,13 @@ function Fabric() {
 	}, [count]);
 
 	return (
-		<div className="fabric_box">
-			<Button onClick={() => handleSetCount()}>{`测试按钮${count}`}</Button>
-		</div>
+		<>
+			<Container>
+				<div className="fabric_box">
+					<Button onClick={() => handleSetCount()}>{`测试按钮${count}`}</Button>
+				</div>
+			</Container>
+		</>
 	);
 }
 
